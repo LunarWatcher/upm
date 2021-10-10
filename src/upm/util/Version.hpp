c20a6cb37e6ad5eed4d711b6b4223bd44205e4c4
Olivia <zoe.i2k1@gmail.com>
2021-10-10 15:42:23 +0200

    Add tests, add version utilities

---
+++ src/upm/util/Version.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <regex>
+#include <string>
+namespace upm {
+
+// TODO: harden for other versioning systems.
+// May need to declare unique version comparators for certain packages,
+// but that assumes they fall outside any logical structure.
+//
+// Problem for later though
+class Version {
+private:
+    static inline std::regex COMPONENT_SPLIT = std::regex("[.-]");
+
+    std::string version;
+    std::vector<std::string> components;
+
+public:
+    Version(const std::string& version);
+
+    const std::string& getVersion();
+
+    friend bool operator<(const Version& a, const Version& b);
+    friend bool operator>(const Version& a, const Version& b);
+    friend bool operator==(const Version& a, const Version& b);
+};
+
+bool operator<(const Version& a, const Version& b);
+bool operator>(const Version& a, const Version& b);
+bool operator==(const Version& a, const Version& b);
+bool operator!=(const Version& a, const Version& b);
+
+}
