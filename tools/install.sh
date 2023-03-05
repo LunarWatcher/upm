27ee37e582dd1a4bd20069234a9f38394c63d7d3
Olivia <zoe.i2k1@gmail.com>
2023-03-05 01:50:13 +0100

    Disable install.sh

--- tools/install.sh
+++ tools/install.sh
@@ -1,41 +1,43 @@
 #!/bin/bash
 
-CMAKE_VERSION=$(cmake --version)
+echo "Defunct due to internal changes"
+exit
 
-echo "Testing cmake..."
-if [ $? -ne 0 ]; then
-    echo "Install CMake and try again"
-    exit -1
-elif [[ "$CMAKE_VERSION" =~ "cmake version ([3-9]\.([6-9]|[1-9][0-9]+)\.[0-9]+|([12][0-9]+|[4-9][0-9]*))" ]]; then
-    echo "Found CMake, but it doesn't appear to be a supported version. Please install CMake 3.10.0 or newer and try again"
-    exit -1
-fi
-echo "$CMAKE_VERSION found and OK"
-echo "Preparing clone directory"
-sudo mkdir -p /opt/upm
-sudo chown $USER -R /opt/upm/
-echo "Cloning repository..."
-git clone --recursive https://github.com/LunarWatcher/upm /opt/upm/_sources
-if [ $? -ne 0 ]; then
-    echo "Failed to clone. If it already exists, remove /opt/upm/_sources and try again."
-    echo "Also note that you can update upm from within itself. This script is not meant for updates."
-    exit -1
-fi
+#CMAKE_VERSION=$(cmake --version)
 
-echo "Clone successful. Entering repo..."
-cd /opt/upm/_sources
+#echo "Testing cmake..."
+#if [ $? -ne 0 ]; then
+    #echo "Install CMake and try again"
+    #exit -1
+#elif [[ "$CMAKE_VERSION" =~ "cmake version ([3-9]\.([6-9]|[1-9][0-9]+)\.[0-9]+|([12][0-9]+|[4-9][0-9]*))" ]]; then
+    #echo "Found CMake, but it doesn't appear to be a supported version. Please install CMake 3.10.0 or newer and try again"
+    #exit -1
+#fi
+#echo "$CMAKE_VERSION found and OK"
+#echo "Preparing clone directory"
+#sudo mkdir -p /opt/upm
+#echo "Cloning repository..."
+#git clone --recursive https://github.com/LunarWatcher/upm /opt/upm/_sources
+#if [ $? -ne 0 ]; then
+    #echo "Failed to clone. If it already exists, remove /opt/upm/_sources and try again."
+    #echo "Also note that you can update upm from within itself. This script is not meant for updates."
+    #exit -1
+#fi
 
-if [ ! -e env ]; then
-    python3 -m venv env
-fi
-source ./env/bin/activate
+#echo "Clone successful. Entering repo..."
+#cd /opt/upm/_sources
 
-echo "Making sure conan is good..."
-python3 -m pip install --upgrade conan || exit -1
+#if [ ! -e env ]; then
+    #sudo python3 -m venv env
+#fi
+#source ./env/bin/activate
 
-echo "Checkout build directory"
-mkdir -p build && cd build
-# Bootstrap build
-cmake .. -DCMAKE_BUILD_TYPE=Release && make -j $(nproc)
-# Make upm install itself
-./bin/upm install upm@nightly
+#echo "Making sure conan is good..."
+#sudo python3 -m pip install --upgrade conan || exit -1
+
+#echo "Checkout build directory"
+#mkdir -p build && cd build
+## Bootstrap build
+#cmake .. -DCMAKE_BUILD_TYPE=Release && make -j $(nproc)
+## Make upm install itself
+#./bin/upm install upm@nightly
